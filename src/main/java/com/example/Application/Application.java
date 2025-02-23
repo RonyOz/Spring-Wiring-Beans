9f984f98e974185dd95e485bd7b52b63ded6553a
Rony Ordoñez <ronyfarido@gmail.com>
2025-02-22 19:17:37 -0500

    Updated to use AppConfig

--- src/main/java/com/example/Application/Application.java
+++ src/main/java/com/example/Application/Application.java
@@ -1,14 +1,15 @@
 package com.example.Application;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.example.config.AppConfig;
 
 public class Application {
 
-    private static final ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+    private static final ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
     public static ApplicationContext getContext() {
         return context;
     }
-
 }
